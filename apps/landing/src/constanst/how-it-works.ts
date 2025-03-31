656f93a9b897f335ac7f4aec01ab561a54c404bc
“Niico4” <nicogarzon131@gmail.com>
2025-03-31 11:53:36 -0500

    feat(ui): implement responsive design

---
+++ apps/landing/src/constanst/how-it-works.ts
@@ -0,0 +1,27 @@
+import BuildingStore from '@icons/building-store.svg';
+import ShoppingCart from '@icons/shopping-cart.svg';
+import Truck from '@icons/truck.svg';
+
+export const HOW_IT_WORKS = [
+  {
+    icon: BuildingStore,
+    title: 'Configura tu tienda',
+    description:
+      'Amplia tu alcance con una tienda online y satisface a tus cliente con un servicio de entrega eficiente y confiable.',
+    badge: '01',
+  },
+  {
+    icon: ShoppingCart,
+    title: 'Recibe pedidos online',
+    description:
+      'Convierte tu tienda en un e-commerce y acepta pedidos de manera rápida y sencilla, sin complicaciones ni límites geográficos.',
+    badge: '02',
+  },
+  {
+    icon: Truck,
+    title: 'Entrega con facilidad',
+    description:
+      'Optimiza la gestión de envíos con una logística eficiente, asegurando entregas rápidas y sin inconvenientes para tus clientes.',
+    badge: '03',
+  },
+];
---
+++ apps/landing/src/constanst/target-businesses.ts
@@ -0,0 +1,26 @@
+import BuildingStore from '@icons/building-store.svg';
+import ShoppingBag from '@icons/shopping-bag.svg';
+import ToolsKitchen from '@icons/tools-kitchen-2.svg';
+
+export const TARGET_BUSINESSES = [
+  {
+    title: 'Tiendas y Supermercados',
+    icon: BuildingStore,
+    description:
+      'Gestiona tu inventario, recibe pedidos online y organiza entregas a domicilio, todo desde una única interfaz.',
+  },
+
+  {
+    title: 'Restaurantes',
+    icon: ToolsKitchen,
+    description:
+      'Recibe pedidos online, gestiona tu menú y coordina tus domiciliarios para entregar los pedidos en tiempo récord.',
+  },
+
+  {
+    title: 'Comercios Minoristas',
+    icon: ShoppingBag,
+    description:
+      'Amplia tu alcance con una tienda online y satisface a tus cliente con un servicio de entrega eficiente y confiable.',
+  },
+];
